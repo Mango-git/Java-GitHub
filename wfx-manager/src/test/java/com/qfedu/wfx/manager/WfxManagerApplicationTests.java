dc2f7ab0f43f072ec00a778131434fdaf017cf7e
Cappuccino <5188059+shunniu@user.noreply.gitee.com>
2019-11-07 20:42:10 +0800

    测试数据上传

---
+++ wfx-manager/src/test/java/com/qfedu/wfx/manager/WfxManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qfedu.wfx.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfxManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
